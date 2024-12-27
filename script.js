981e9db073b6807e35bcc7fd86ad9a6dcdec3d29
Suvajit Saha <suvajit@itobuz.com>
2024-12-27 10:55:27 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("#button-click");
 
 const changeColor = () => {
-  const value = Math. floor(Math. random() * (999999 -100000 + 1)) + 100000
+  const value = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
   console.log(value);
 
   document.body.style.backgroundColor = `#${value}`;
